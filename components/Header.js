8fb8422d1927a8318b6f8e5ee69572bdcc2de06c
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2022-09-05 00:48:21 +0530

    colour 1

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <nav className="p-5 border-b-2 bg-[#20109c] flex flex-row">
+        <nav className="p-5 border-b-2 bg-[#100c08] flex flex-row">
             <h1 className="py-4 px-4 font-bold text-3xl bg-[#aafe1e]"> RecycloMade </h1>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton moralisAuth={false}/>
