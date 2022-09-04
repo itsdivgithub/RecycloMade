fc7402c6963d9a83ccf76c6eb8c18eae692fceea
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2022-09-05 00:54:35 +0530

    connectbuttonbg

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <nav className="p-5 border-b-2 bg-[#100c08] flex flex-row">
             <h1 className="py-4 px-4 font-bold text-3xl bg-[#aafe1e]"> RecycloMade </h1>
-            <div className="ml-auto py-2 px-4">
+            <div className="ml-auto py-4 px-4 bg-[#aafe1e]">
                 <ConnectButton moralisAuth={false}/>
             </div>
         </nav>
